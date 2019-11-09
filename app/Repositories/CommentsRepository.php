3a45febec5a4bb28f4398902fa9512d151426300
vadim50 <waswas4@yandex.ru>
2019-11-09 15:35:14 +0300

    Lesson 68(Урок 14.Правый сайд-бар раздела блог)

---
+++ app/Repositories/CommentsRepository.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace App\Repositories;
+
+use App\Comment;
+
+class CommentsRepository extends Repository {
+
+	public function __construct(Comment $comment){
+		$this->model = $comment;
+	}
+
+}
+
+
+ ?>
--- config/settings.php
+++ config/settings.php
@@ -5,7 +5,9 @@ return [
 	'slider_path'=>'slider-cycle',
 	'home_port_count'=>5,
 	'home_articles_count'=>3,
-	'paginate'=>2
+	'paginate'=>2,
+	'resent_comments'=>3,
+	'resent_portfolios'=>3
 
 ];
 
